daa01424763349ce80c7ca1a57f372f381c8a538
The Tien Mai <39219912+tienmt@users.noreply.github.com>
2025-11-16 14:37:54 +0100

    Add files via upload

---
+++ doc/tutorial.R
@@ -0,0 +1,13 @@
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  fig.width = 7,
+  fig.height = 4.5
+)
+library(tobitbayes)
+set.seed(1)
+
+## -----------------------------------------------------------------------------
+sessionInfo()
+
